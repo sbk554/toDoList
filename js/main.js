05343dc0fd6d29b4cc1d6fe6f9780678dac33e72
sbk554 <sbk8731@naver.com>
2025-02-12 10:57:27 +0900

    hi

--- js/main.js
+++ js/main.js
@@ -1,62 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
     updateListDisplay();
+
+    document.querySelector("#modalAdd").addEventListener("click", function () {
+        document.querySelector(".list-wrap").setAttribute("style","display:none;")
+        document.querySelector(".list-add-wrap").setAttribute("style","display:flex;")
+    });
+
+    document.querySelector("#modalClose").addEventListener("click", function () {
+        document.querySelector(".list-wrap").setAttribute("style","display:block;")
+        document.querySelector(".list-add-wrap").setAttribute("style","display:none;")
+
+        updateListDisplay();
+    });
+
+    document.querySelector("#listAddSubmit").addEventListener("click", function () {
+        var listAddText = document.getElementById("listAddText").value;
+        var getLocalSt = localStorage.getItem("list");
+
+        // localStorage 값이 null이면 빈 배열로 초기화
+        var listArray = getLocalSt ? JSON.parse(getLocalSt) : [];
+
+        // 배열 길이를 pk 값으로 사용
+        var list = {
+            pk: listArray.length,
+            title: listAddText,
+            ck: "N"
+        };
+
+        // 리스트에 새 항목 추가
+        listArray.push(list);
+
+        // localStorage에 저장
+        localStorage.setItem("list", JSON.stringify(listArray));
+        document.getElementById("listAddText").value = "";
+    });
+
+    //list 제거
+    document.querySelectorAll(".listDel").forEach(function(item){
+        item.addEventListener("click", function () {
+            var num = Number(item.getAttribute("num"));
+            var getLocalStList = localStorage.getItem("list");
+            var listArray = getLocalStList ? JSON.parse(getLocalStList) : [];
+
+            // pk가 num과 일치하는 항목을 제외한 배열 생성
+            listArray = listArray.filter(function(subItem) {
+                return subItem.pk !== num; // pk가 num과 다르면 새로운 배열에 포함
+            });
+
+            // pk를 0부터 다시 순차적으로 할당
+            listArray = listArray.map(function(subItem, index) {
+                return { ...subItem, pk: index };
+            });
+            console.log(listArray)
+            // 변경된 배열을 localStorage에 다시 저장
+            localStorage.setItem("list", JSON.stringify(listArray));
+
+            updateListDisplay();
+        });
+    })
 });
-
-function modalAdd(){
-    document.querySelector(".list-wrap").setAttribute("style","display:none;")
-    document.querySelector(".list-add-wrap").setAttribute("style","display:flex;")
-}
-
-function modalClose(){
-    document.querySelector(".list-wrap").setAttribute("style","display:block;")
-    document.querySelector(".list-add-wrap").setAttribute("style","display:none;")
-
-    updateListDisplay();
-}
-
-//list 추가
-function listAddSubmit(){
-    var listAddText = document.getElementById("listAddText").value;
-    var getLocalSt = localStorage.getItem("list");
-
-    // localStorage 값이 null이면 빈 배열로 초기화
-    var listArray = getLocalSt ? JSON.parse(getLocalSt) : [];
-
-    // 배열 길이를 pk 값으로 사용
-    var list = {
-        pk: listArray.length,
-        title: listAddText,
-        ck: "N"
-    };
-
-    // 리스트에 새 항목 추가
-    listArray.push(list);
-
-    // localStorage에 저장
-    localStorage.setItem("list", JSON.stringify(listArray));
-    document.getElementById("listAddText").value = "";
-}
-
-//list 제거
-function listDel(num){
-    var getLocalStList = localStorage.getItem("list");
-    var listArray = getLocalStList ? JSON.parse(getLocalStList) : [];
-
-    // pk가 num과 일치하는 항목을 제외한 배열 생성
-    listArray = listArray.filter(function(item) {
-        return item.pk !== num; // pk가 num과 다르면 새로운 배열에 포함
-    });
-
-    // pk를 0부터 다시 순차적으로 할당
-    listArray = listArray.map(function(item, index) {
-        return { ...item, pk: index };
-    });
-
-    // 변경된 배열을 localStorage에 다시 저장
-    localStorage.setItem("list", JSON.stringify(listArray));
-    
-    updateListDisplay();
-}
 
 //list html 생성
 function updateListDisplay() {
@@ -71,11 +73,11 @@ function updateListDisplay() {
                     <div class="list-item">
                         <input type="checkbox" class="task-checkbox" value="${item.pk}" ${item.ck === "Y" ? "checked" : ""}/>
                         <span style="text-decoration:${item.ck === "Y" ? "line-through" : "none"}">${item.title}</span>
-                        <button onclick="listDel(${item.pk})">&#128465;</button>
+                        <button class="listDel" num="${item.pk}">&#128465;</button>
                     </div>
                     `;
     });
-
+    //<button class="listDel" onclick="listDel(${item.pk})">&#128465;</button>
     document.getElementById("list-content").innerHTML = resultHtml;
     checkBoxYn();
 };
@@ -97,6 +99,7 @@ function checkBoxYn(){
                 }
             });
             
+            //checked 된것들은 아래로 내려보내게 순서정렬
             listArray.sort(function(a, b) {
                 if (a.ck === 'Y' && b.ck !== 'Y') return 1;
                 if (a.ck !== 'Y' && b.ck === 'Y') return -1;
